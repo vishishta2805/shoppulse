5471ce6c075138318b1ba3bbc68901f8bf4c5ab0
Vishishta Reddy <vishishta2805@gmail.com>
2026-04-23 00:41:36 +0530

    update API URL for production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://shoppulse-api-kptx.onrender.com',
   timeout: 60000,
   headers: { 'Content-Type': 'application/json' },
 })
