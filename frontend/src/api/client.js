93dd8d2a6abb4f2ee13f34a8c20b03df8f3ac07f
Vishishta Reddy <vishishta2805@gmail.com>
2026-04-22 23:18:50 +0530

    ready for deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://shoppulse-api-oave.onrender.com',
+  baseURL: 'http://localhost:8000',
   timeout: 60000,
   headers: { 'Content-Type': 'application/json' },
 })
